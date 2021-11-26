c01bc7e41ad0337301b0a895404e89ca56507a19
Sango <45958488+ns251632@users.noreply.github.com>
2021-11-26 13:43:16 +0900

    Update func_saveTable.R

--- func_saveTable.R
+++ func_saveTable.R
@@ -1,6 +1,6 @@
 #gtsummaryで作成した表の保存
 save_tbl <- function(x, y){
-  name = y
+  name <- y
   dir <- getwd()               #Working Dir
   folder <- "1_tbl"            #保存するフォルダ
   file <- paste(name, ".png")  #保存名 + 拡張子(.csv)

0651470601fcd77411b65d4e729d93c20d70187f
Sango <45958488+ns251632@users.noreply.github.com>
2021-11-26 13:34:43 +0900

    Create saveFig_func.R

---
+++ saveFig_func.R
@@ -0,0 +1,11 @@
+#ggplot2で作成した図の保存
+save_fig <- function(x, y){
+  name = y
+  dir <- getwd()               #Working Dir
+  folder <- "2_fig"            #保存するフォルダ
+  file <- paste(name, ".png")  #保存名 + 拡張子(.csv)
+  result <- ggplot2::ggsave(
+    file = paste(dir, folder, file, sep = "/"), plot = x
+  )
+  result
+}
